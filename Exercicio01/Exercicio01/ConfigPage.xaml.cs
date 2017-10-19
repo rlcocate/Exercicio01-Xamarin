bf35188e5b3e52a82c931e0e2096eeb4537c8d9a
Rodrigo Cocate <rodrigo.cocate@hotmail.com>
2017-10-18 23:14:03 -0200

    exercicio 1

---
+++ Exercicio01/Exercicio01/ConfigPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Exercicio01
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ConfigPage : ContentPage
+    {
+        public ConfigPage()
+        {            
+            InitializeComponent();
+        }
+
+        private void swcEmail_OnChanged(object sender, ToggledEventArgs e)
+        {
+            txtTexto.IsEnabled = swcEmail.On;
+            if (!swcEmail.On) this.txtTexto.Text = "";
+        }
+    }
+}
---
+++ Exercicio01/Exercicio01/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Exercicio01
+{
+    public partial class MainPage : ContentPage
+    {
+        Entry entryTexto;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnviar_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnConfiguracao_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ConfigPage());
+        }
+    }
+}
